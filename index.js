aa8e73c9d5ea2f80ed3ad9504f8489e03eaf16bf
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-31 20:54:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function theBeatlesPlay(musicianArray, instrumentArray) {
   answerArray = [];
-  for(i = 0; i < musicianArray.length; i ++)
+  for(i = 0; i < musicianArray.length; i ++) {
+    answerArray.push(`musicianArray[i]` + `instrumentArray[i]`)
+  }
 }
